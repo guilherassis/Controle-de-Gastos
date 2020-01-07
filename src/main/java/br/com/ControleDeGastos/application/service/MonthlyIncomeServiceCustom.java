c5723df92df00a4aec562261c9576700c45bd455
guilherassis <guilher.assis@gmail.com>
2020-01-07 19:46:47 -0300

    Create Service MonthlyIncome(findById,findAll,save,delete)

--- src/main/java/br/com/ControleDeGastos/application/service/MonthlyIncomeServiceCustom.java
+++ src/main/java/br/com/ControleDeGastos/application/service/MonthlyIncomeServiceCustom.java
@@ -1,4 +1,16 @@
 package br.com.ControleDeGastos.application.service;
 
+import br.com.ControleDeGastos.application.domain.entity.MonthlyIncome;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface MonthlyIncomeServiceCustom {
+
+    void delete(Long id);
+    MonthlyIncome save(MonthlyIncome monthlyIncome);
+    List<MonthlyIncome> findByAll();
+    Optional<MonthlyIncome> findbyId(Long id);
+
+
 }
--- src/main/java/br/com/ControleDeGastos/application/service/impl/MonthlyIncomeServiceImpl.java
+++ src/main/java/br/com/ControleDeGastos/application/service/impl/MonthlyIncomeServiceImpl.java
@@ -1,8 +1,42 @@
 package br.com.ControleDeGastos.application.service.impl;
 
+import br.com.ControleDeGastos.application.domain.dao.MonthlyIncomeRepository;
+import br.com.ControleDeGastos.application.domain.entity.MonthlyIncome;
 import br.com.ControleDeGastos.application.service.MonthlyIncomeServiceCustom;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MonthlyIncomeServiceImpl implements MonthlyIncomeServiceCustom {
+
+    @Autowired
+    MonthlyIncomeRepository monthlyIncomeRepository;
+
+    @Transactional
+    public Optional<MonthlyIncome> findbyId(Long id){
+        return monthlyIncomeRepository.findById(id);
+
+    }
+
+    @Transactional
+    public List<MonthlyIncome> findByAll(){
+        return monthlyIncomeRepository.findAll();
+
+    }
+
+    @Transactional
+    public MonthlyIncome save(MonthlyIncome monthlyIncome){
+        return monthlyIncomeRepository.save(monthlyIncome);
+
+    }
+
+    @Transactional
+    public void delete(Long id){
+        monthlyIncomeRepository.deleteById(id);
+    }
+
 }
