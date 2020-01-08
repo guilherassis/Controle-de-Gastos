47c32f7baba4860d09ce2c0a1577de2ee599bf36
guilherassis <guilher.assis@gmail.com>
2020-01-07 23:37:06 -0300

    Create Service PaymentType(findById,findAll,save,delete)

--- src/main/java/br/com/ControleDeGastos/application/service/PaymentTypeServiceCustom.java
+++ src/main/java/br/com/ControleDeGastos/application/service/PaymentTypeServiceCustom.java
@@ -1,5 +1,17 @@
 package br.com.ControleDeGastos.application.service;
 
 
+import br.com.ControleDeGastos.application.domain.entity.PaymentType;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
+
 public interface PaymentTypeServiceCustom {
+
+    Optional<PaymentType> findById(Long id);
+    List<PaymentType> findAll();
+    PaymentType save(PaymentType paymentType);
+    void deleteById(Long id);
+
 }
--- src/main/java/br/com/ControleDeGastos/application/service/impl/PaymentTypeServiceImpl.java
+++ src/main/java/br/com/ControleDeGastos/application/service/impl/PaymentTypeServiceImpl.java
@@ -1,8 +1,42 @@
 package br.com.ControleDeGastos.application.service.impl;
 
+import br.com.ControleDeGastos.application.domain.dao.PaymentTypeRepository;
+import br.com.ControleDeGastos.application.domain.entity.PaymentType;
 import br.com.ControleDeGastos.application.service.PaymentTypeServiceCustom;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PaymentTypeServiceImpl implements PaymentTypeServiceCustom {
+
+    @Autowired
+    PaymentTypeRepository paymentTypeRepository;
+
+    @Transactional
+    public Optional<PaymentType> findById(Long id){
+        return paymentTypeRepository.findById(id);
+
+    }
+
+    @Transactional
+    public List<PaymentType> findAll(){
+        return paymentTypeRepository.findAll();
+
+    }
+
+    @Transactional
+    public PaymentType save(PaymentType paymentType){
+       return paymentTypeRepository.save(paymentType);
+    }
+
+    @Transactional
+    public void deleteById(Long id){
+        paymentTypeRepository.deleteById(id);
+
+    }
+
 }
