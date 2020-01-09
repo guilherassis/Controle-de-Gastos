3aa98e9f2885374af491b4a7a42fed9308ef669b
guilherassis <guilher.assis@gmail.com>
2020-01-09 00:30:49 -0300

    Create Service Prospection(findById,findAll,save,delete)

--- src/main/java/br/com/ControleDeGastos/application/service/ProspectionServiceCustom.java
+++ src/main/java/br/com/ControleDeGastos/application/service/ProspectionServiceCustom.java
@@ -1,4 +1,16 @@
 package br.com.ControleDeGastos.application.service;
 
+import br.com.ControleDeGastos.application.domain.entity.Prospection;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
+
 public interface ProspectionServiceCustom {
+
+    Optional<Prospection> findById(Long id);
+    List<Prospection> findAll();
+    void delete(Long id);
+    Prospection save(Prospection prospection);
+
 }
--- src/main/java/br/com/ControleDeGastos/application/service/impl/ProspectionServiceImpl.java
+++ src/main/java/br/com/ControleDeGastos/application/service/impl/ProspectionServiceImpl.java
@@ -1,8 +1,44 @@
 package br.com.ControleDeGastos.application.service.impl;
 
+import br.com.ControleDeGastos.application.domain.dao.ProspectionRepository;
+import br.com.ControleDeGastos.application.domain.entity.Prospection;
 import br.com.ControleDeGastos.application.service.ProspectionServiceCustom;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProspectionServiceImpl implements ProspectionServiceCustom {
+
+    @Autowired
+    ProspectionRepository prospectionRepository;
+
+    @Transactional
+    public Optional<Prospection> findById(Long id){
+        return prospectionRepository.findById(id);
+
+    }
+
+    @Transactional
+    public List<Prospection> findAll(){
+        return prospectionRepository.findAll();
+
+    }
+
+    @Transactional
+    public void delete(Long id){
+        prospectionRepository.deleteById(id);
+
+    }
+
+    @Transactional
+    public Prospection save(Prospection prospection){
+       return prospectionRepository.save(prospection);
+
+    }
+
+
 }
