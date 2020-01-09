97a299bdf36a32f8184c73e67722a12bda6a5e79
guilherassis <guilher.assis@gmail.com>
2020-01-09 00:41:37 -0300

    Create Service User(findById,findAll,save,delete)

--- src/main/java/br/com/ControleDeGastos/application/service/UserServiceCustom.java
+++ src/main/java/br/com/ControleDeGastos/application/service/UserServiceCustom.java
@@ -1,4 +1,15 @@
 package br.com.ControleDeGastos.application.service;
 
+import br.com.ControleDeGastos.application.domain.entity.User;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface UserServiceCustom {
+
+    Optional<User> findById(Long id);
+    List<User> findAll();
+    void delete(Long id);
+    User save(User user);
+
 }
--- src/main/java/br/com/ControleDeGastos/application/service/impl/UserServiceImpl.java
+++ src/main/java/br/com/ControleDeGastos/application/service/impl/UserServiceImpl.java
@@ -1,8 +1,38 @@
 package br.com.ControleDeGastos.application.service.impl;
 
+import br.com.ControleDeGastos.application.domain.dao.UserRepository;
+import br.com.ControleDeGastos.application.domain.entity.User;
 import br.com.ControleDeGastos.application.service.UserServiceCustom;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserServiceCustom {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public Optional<User> findById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    @Override
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public void delete(Long id) {
+        userRepository.deleteById(id);
+
+    }
+
+    @Override
+    public User save(User user) {
+        return userRepository.save(user);
+    }
 }
